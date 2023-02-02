563a07def5ac0f9cc2247a4869b8e9d214d2d82d
xlSaladix <neyencoria@gmail.com>
2023-02-02 17:46:23 -0300

    Modificacion de las cards de Mis Proyectos

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,22 +1,27 @@
-import { StmtModifier } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
+
+
 export class PortfolioComponent implements OnInit {
   basic: string = 'Basic';
+  faXmark = faXmark;
 
   constructor(public modal: NgbModal) { }
 
   ngOnInit(): void {
   }
 
-  openCentered(contenido: any) {
+  openCenteredSM(contenido: any) {
     this.modal.open(contenido, { centered: true, size: 'sm' });
+  }
+  openCenteredLG(contenido: any) {
+    this.modal.open(contenido, { centered: true, size: 'lg' });
   }
 }
